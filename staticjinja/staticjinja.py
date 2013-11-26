d7df407fd0735890d9f99e792ab1fab6ba9d9dee
Ceasar Bautista <cbautista2010@gmail.com>
2013-11-25 23:21:18 -0500

    factor out get_context_generator
    Adds another test seam.

--- staticjinja/staticjinja.py
+++ staticjinja/staticjinja.py
@@ -62,7 +62,19 @@ class Renderer(object):
         """
         return self._env.get_template(template_name)
 
-    def get_context(self, template_name):
+    def get_context_generator(self, template_name):
+        """Get a context generator for a template.
+        
+        Raises a ValueError if no matching context generator can be found.
+        
+        :param template_name: the name of the template
+        """
+        for regex, context_generator in self.contexts:
+            if re.match(regex, template_name):
+                return context_generator
+        raise ValueError("no matching context generator")
+
+    def get_context(self, template):
         """Get the context for a template.
 
         By default, this function will return an empty context.
@@ -71,22 +83,27 @@ class Renderer(object):
         template and executed. The context generator should return a dictionary
         representing the context.
 
-        :param template_name: the name of the template
+        :param template: the template to get the context for
         """
-        for regex, context_generator in self.contexts:
-            if re.match(regex, template_name):
-                try:
-                    return context_generator(self.get_template(template_name))
-                except TypeError:
-                    return context_generator()
-        return {}
+        try:
+            context_generator = self.get_context_generator(template.name)
+        except ValueError:
+            return {}
+        else:
+            try:
+                return context_generator(template)
+            except TypeError:
+                return context_generator()
 
     def get_rule(self, template_name):
         """Find a matching compilation rule for a function.
         
-        Raises a ValueError if not matching rule can be found."""
+        Raises a ValueError if no matching rule can be found.
+        
+        :param template_name: the name of the template
+        """
         for regex, render_func in self.rules:
-            if re.match(regex, template.name):
+            if re.match(regex, template_name):
                 return render_func
         raise ValueError("no matching rule")
 
@@ -151,7 +168,7 @@ class Renderer(object):
         """Render each of the templates."""
         for template_name in self.template_names:
             template = self.get_template(template_name)
-            context = self.get_context(template_name)
+            context = self.get_context(template)
             self.render_template(template, context)
 
     def _watch(self):
@@ -170,7 +187,7 @@ class Renderer(object):
                         self.render_templates()
                     elif self.is_template(filename):
                         template = self.get_template(filename)
-                        context = self.get_context(filename)
+                        context = self.get_context(template)
                         self.render_template(filename)
         easywatch.watch(self.searchpath, handler)
 
@@ -194,18 +211,26 @@ def make_renderer(searchpath="templates",
                  ):
     """Get a Renderer object.
 
-    :param searchpath: the directory to search for templates. Defaults to
-                       ``'templates'``
-    :param outpath: the directory to store the rendered files in
-    :param contexts: list of regex-function pairs. the function should return a
-                     context for that template. the regex, if matched against
-                     a filename, will cause the context to be used.
-    :param rules: used to override template compilation. The value of rules
-                  should be a list of `regex, function` pairs where `function`
-                  takes a jinja2 Environment, the filename, and the context and
-                  renders the template, and `regex` is a regex that if matched
-                  against a filename will cause `function` to be used instead
-                  of the default.
+    :param searchpath: the name of the directory to search for templates.
+    
+                       Defaults to 'templates'.
+    :param outpath: the name of the directory to store the rendered files in
+
+                    Defaults to `.`.
+    :param contexts: list of `(regex, function)` pairs.
+    
+                     If `regex` matches a template's name, `function` will
+                     be invoked and expected to provide a context.
+
+                     `function` should optionally take a Template as a
+                     parameter and return a dictionary context when invoked.
+    :param rules: list of `(regex, function)` pairs.
+
+                  If `regex` matches a template's name, rendering will
+                  delegate to `function`.
+    
+                  `function` should take a jinja2 Environment, a filename, and
+                  a context and render the template.
     :param encoding: the encoding of templates to use. Defaults to 'utf8'
     :param extensions: list of extensions to add to the Environment
     """
