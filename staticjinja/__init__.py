4c0a7a438bfca09806887f34e0fc7b0f1037bac8
NickCrews <nicholas.b.crews@gmail.com>
2020-11-10 11:48:18 -0700

    Add __doc__ to __init__.py
    The docstring for the package was not very helpful. Now you get some more useful info and afew links.

--- staticjinja/__init__.py
+++ staticjinja/__init__.py
@@ -1,4 +1,20 @@
-# -*- coding:utf-8 -*-
+"""
+staticjinja
+===========
+
+staticjinja is a library that makes it easy to build static sites using Jinja2.
+
+Many static site generators are complex, with long manuals and unnecessary
+features. But using template engines to build static websites is really useful.
+staticjinja is designed to be lightweight (under 500 lines of source code), and
+to be easy to use, learn, and extend, enabling you to focus on making your site.
+
+Documentation is available at http://staticjinja.readthedocs.org/en/latest/.
+
+Please file bugs, view source code, and contribute at
+https://github.com/staticjinja/staticjinja/
+"""
+
 # flake8: noqa
 
 from __future__ import absolute_import
