03bac7e3b9586ddefebfd8730d6c5a9770e958ee
NickCrews <nicholas.b.crews@gmail.com>
2020-11-10 10:46:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(
     name="staticjinja",
     version=versions["__version__"],
     description="jinja based static site generator",
-    author="Ceasar Bautista",
-    author_email="cbautista2010@gmail.com",
-    url="https://github.com/Ceasar/staticjinja",
+    author="Ceasar Bautista, Nick Crews",
+    author_email="cbautista2010@gmail.com, nicholas.b.crews@gmail.com",
+    url="https://github.com/staticjinja/staticjinja",
     keywords=["jinja", "static", "website"],
     packages=["staticjinja"],
     entry_points={
@@ -35,6 +35,10 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Development Status :: 4 - Beta",
     ]
