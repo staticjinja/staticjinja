44846eb85de92d8bb52c6c566c4d5018bb3eda27
NickCrews <nicholas.b.crews@gmail.com>
2020-11-12 10:00:39 -0700

    (#87) Add markdown example
    This is the start of a fix for https://github.com/staticjinja/staticjinja/issues/87, where the current markdown rendering example is totally outdated.
    
    Instead of just fixing the examples embedded in the docs, I wanted to give users a fully featured example that they could run straightaway or copy adn modify. This format of examples folder could also could be used for tests, not sure yet...

---
+++ examples/markdown/build.py
@@ -0,0 +1,42 @@
+# build.py
+#!/usr/bin/env python3
+import os
+import jinja2
+
+# Markdown to HTML library
+# https://pypi.org/project/Markdown/
+import markdown
+
+from staticjinja import Site
+
+markdowner = markdown.Markdown(output_format="html5")
+def md_context(template):
+    with open(template.filename) as f:
+        markdown_content = f.read()
+        return {'post_content_html': markdowner.convert(markdown_content)}
+
+def render_md(site, template, **kwargs):
+    # Given a template such as posts/post1.md
+    # Determine the post's title (post1) and it's directory (posts/)
+    directory, fname = os.path.split(template.name)
+    post_title, _ = fname.split(".")
+
+    # Determine where the result will be streamed (build/posts/post1.html)
+    out_dir = os.path.join(site.outpath, directory)
+    post_fname = "{}.html".format(post_title)
+    out = os.path.join(out_dir, post_fname)
+
+    # Render and stream the result
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    post_template = site.get_template("_post.html")
+    post_template.stream(**kwargs).dump(out, encoding="utf-8")
+
+site = Site.make_site(
+        searchpath='src',
+        outpath='build',
+        contexts=[('.*.md', md_context)],
+        rules = [('.*.md', render_md)],
+        )
+
+site.render()
---
+++ examples/markdown/src/posts/post2.md
@@ -0,0 +1,7 @@
+# Todo:
+
+1. Look good
+    1. You already look _fabulous_.
+    2. Wear [Hawaiian shirts](https://en.wikipedia.org/wiki/Aloha_shirt).
+2. Feel good
+3. Look good
