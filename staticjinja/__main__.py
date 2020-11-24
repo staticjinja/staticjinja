8e954479861699e0635414079e0017c15c68964e
NickCrews <nicholas.b.crews@gmail.com>
2020-11-23 18:08:11 -0700

    Fix __main__.py to use Site.make_site, not staticjinja.make_site

--- staticjinja/__main__.py
+++ staticjinja/__main__.py
@@ -3,9 +3,9 @@
 
 import os
 
-from . import staticjinja
+from .staticjinja import Site
 
 if __name__ == "__main__":
     searchpath = os.path.join(os.getcwd(), 'templates')
-    site = staticjinja.make_site(searchpath=searchpath)
+    site = Site.make_site(searchpath=searchpath)
     site.render(use_reloader=True)
