5c43f29521833dc1bf48da35a08c764322bbbc06
NickCrews <nicholas.b.crews@gmail.com>
2020-12-03 08:13:04 -0700

    Parameterize the CLI srcpath tests
    The three existing CLI tests ensure that the various `--srcpath` args given to the CLI result in Site.make_site() being called with the correct searchpath parameter. Parameterize these three tests into one.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,65 +1,33 @@
 import unittest.mock as mock
 
+import pytest
+
 from staticjinja import cli
 
+srcpath_cases = [(None, '/templates'),  # Default is CWD/templates/
+                 ('src', '/src'),  # Relative paths assumed to be under CWD
+                 ('/foo/src', '/foo/src'),  # Absolute paths taken as-is
+                 ]
 
+
+@pytest.mark.parametrize("srcpath, expected", srcpath_cases)
 @mock.patch('os.path.isdir')
 @mock.patch('os.getcwd')
 @mock.patch('staticjinja.cli.Site.make_site')
-def test_cli_srcpath(mock_make_site, mock_getcwd, mock_isdir):
+def test_srcpath(mock_make_site, mock_getcwd, mock_isdir, srcpath, expected):
+    '''Test that various `--srcpath` args given to the CLI result in
+    Site.make_site() being called with the correct searchpath parameter.'''
     mock_isdir.return_value = True
     mock_getcwd.return_value = '/'
-
     cli.render({
-        '--srcpath': 'templates',
+        '--srcpath': srcpath,
         '--outpath': None,
         '--static': None,
         'watch': False,
     })
 
     mock_make_site.assert_called_once_with(
-        searchpath='/templates',
-        outpath='/',
-        staticpaths=None
-    )
-
-
-@mock.patch('os.path.isdir')
-@mock.patch('os.getcwd')
-@mock.patch('staticjinja.cli.Site.make_site')
-def test_cli_srcpath_default(mock_make_site, mock_getcwd, mock_isdir):
-    mock_isdir.return_value = True
-    mock_getcwd.return_value = '/'
-
-    cli.render({
-        '--srcpath': None,
-        '--outpath': None,
-        '--static': None,
-        'watch': False,
-    })
-
-    mock_make_site.assert_called_once_with(
-        searchpath='/templates',
-        outpath='/',
-        staticpaths=None
-    )
-
-
-@mock.patch('os.path.isdir')
-@mock.patch('os.getcwd')
-@mock.patch('staticjinja.cli.Site.make_site')
-def test_cli_srcpath_absolute(mock_make_site, mock_getcwd, mock_isdir):
-    mock_isdir.return_value = True
-    mock_getcwd.return_value = '/'
-    cli.render({
-        '--srcpath': '/foo/templates',
-        '--outpath': None,
-        '--static': None,
-        'watch': False,
-    })
-
-    mock_make_site.assert_called_once_with(
-        searchpath='/foo/templates',
+        searchpath=expected,
         outpath='/',
         staticpaths=None
     )
