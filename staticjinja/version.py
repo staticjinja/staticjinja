320a337c9184a2785c5df08b04447a447f129858
NickCrews <nicholas.b.crews@gmail.com>
2021-01-22 17:33:47 -0800

    Pass token to `actions/create-release@v1` in publish workflow
    It errored in https://github.com/staticjinja/staticjinja/runs/1752031421?check_suite_focus=true because it didn't receive it.

--- staticjinja/version.py
+++ staticjinja/version.py
@@ -4,5 +4,5 @@ when we try to read the version in setup.py, we are forced to import all the
 dependencies in __init__.py, which are probably not installed yet. We can
 import this version info from __init__.py as needed.
 """
-__version_info__ = (1, 0, 1)
+__version_info__ = (1, 0, 2)
 __version__ = '.'.join(map(str, __version_info__))
