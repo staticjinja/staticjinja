052f34bf5573fbd964fdd7ad7bda1ecc2ed32903
Nick Crews <nicholas.b.crews@gmail.com>
2021-03-16 12:16:22 -0600

    tests: Move Reloader tests to separate file
    We now need to use `conftest.py` to define all of our common
    fixtures and utilities.
    
    I also plan on further splitting up the
    huge `site()` fixture into smaller components and steps,
    so each test doesn't have way more context than it needs.

--- tests/test_staticjinja.py
+++ tests/test_staticjinja.py
@@ -1,62 +1,6 @@
-from pathlib import Path
-
-from pytest import fixture, mark, raises
+from pytest import mark, raises
 
 from staticjinja import Site, Reloader
-
-
-@fixture
-def root_path(tmpdir):
-    return tmpdir
-
-
-@fixture
-def template_path(root_path):
-    return root_path.mkdir("templates")
-
-
-@fixture
-def build_path(root_path):
-    return root_path.mkdir("build")
-
-
-@fixture
-def site(template_path, build_path):
-    template_path.join(".ignored1.html").write("Ignored 1")
-    template_path.join("_partial1.html").write("Partial 1")
-    template_path.join("template1.html").write("Test 1")
-    template_path.join("template2.html").write("Test 2")
-    template_path.mkdir("sub").join("template3.html").write("Test {{b}}")
-    template_path.mkdir("sub1").join(".ignored2.html").write("Ignored 2")
-    template_path.mkdir("sub2").join("_partial2.html").write("Partial 2")
-    template_path.mkdir(".ignoreds").join("ignored3.html").write("Ignored 3")
-    template_path.mkdir("_partials").join("partial3.html").write("Partial 3")
-    template_path.join("template4.html").write("Test {{b}} and {{c}}")
-    template_path.mkdir("static_css").join("hello.css").write("a { color: blue; }")
-    template_path.mkdir("static_js").join("hello.js").write(
-        "var a = function () {return true};"
-    )
-    template_path.join("favicon.ico").write("Fake favicon")
-    contexts = [
-        ("template2.html", lambda t: {"a": 1}),
-        (".*template3.html", lambda: {"b": 3}),
-        ("template4.html", {"b": 4, "c": 5}),
-        (".*[4-9].html", {"c": 6}),
-    ]
-    rules = [
-        ("template2.html", lambda env, t, a: None),
-    ]
-    return Site.make_site(
-        searchpath=str(template_path),
-        outpath=str(build_path),
-        contexts=contexts,
-        rules=rules,
-    )
-
-
-@fixture
-def reloader(site):
-    return Reloader(site)
 
 
 def test_template_names(site):
@@ -160,42 +104,6 @@ def test_with_reloader(monkeypatch, site):
 
     site.render(use_reloader=True)
     assert watch_called
-
-
-def test_should_handle(reloader, root_path, template_path):
-    exists = template_path / "template1.html"
-    DNE = template_path / "DNE.html"
-    assert reloader.should_handle("created", str(exists))
-    assert reloader.should_handle("modified", str(exists))
-    assert not reloader.should_handle("deleted", str(exists))
-    assert not reloader.should_handle("modified", str(DNE))
-
-
-def test_event_handler(monkeypatch, reloader, template_path):
-    rendered = []
-
-    def fake_renderer(template, context=None, filepath=None):
-        rendered.append(template)
-
-    monkeypatch.setattr(reloader.site, "render_template", fake_renderer)
-
-    template1_path = str(template_path.join("template1.html"))
-    reloader.event_handler("modified", template1_path)
-    assert rendered == [reloader.site.get_template("template1.html")]
-
-
-def test_event_handler_static(monkeypatch, reloader, template_path):
-    copied_paths = []
-
-    def fake_copy_static(files):
-        copied_paths.extend(Path(f) for f in files)
-
-    monkeypatch.setattr(reloader.site, "copy_static", fake_copy_static)
-
-    reloader.site.staticpaths = ["static_css"]
-    css_path = Path("static_css") / "hello.css"
-    reloader.event_handler("modified", template_path / css_path)
-    assert copied_paths == [css_path]
 
 
 is_ignored_cases = [
